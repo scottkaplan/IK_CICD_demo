774b04bfce866fc25de2f79cb43d04159b32b3fb
Scott Kaplan <scott@kaplans.com>
2023-12-23 22:36:48 +0000

    From https://antonputra.com/terraform/how-to-create-eks-cluster-using-terraform

---
+++ terraform/6-eks.tf
@@ -0,0 +1,46 @@
+resource "aws_iam_role" "demo" {
+  name = "eks-cluster-demo"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.demo.name
+}
+
+variable "cluster_name" {
+  default = "demo"
+  type = string
+  description = "AWS EKS CLuster Name"
+  nullable = false
+}
+
+resource "aws_eks_cluster" "demo" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.demo.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.private-us-west-1a.id,
+      aws_subnet.private-us-west-1c.id,
+      aws_subnet.public-us-west-1a.id,
+      aws_subnet.public-us-west-1c.id
+    ]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy]
+}
