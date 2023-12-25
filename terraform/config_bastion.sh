40dbb5db268761578f642ccbe85335b6f0f0107f
Scott Kaplan <scott@kaplans.com>
2023-12-25 19:13:34 +0000

    Debugging: which host are we on?

---
+++ terraform/config_bastion.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+uname -a
+sudo /usr/bin/yum -y install ansible
+mkdir /tmp/ansible
+for f in packages dot_files kubectl jenkins; do
+    /usr/bin/wget -O /tmp/ansible/${f}.yaml https://raw.githubusercontent.com/scottkaplan/IK_CICD_demo/main/ansible/${f}.yaml
+    sudo ansible-playbook /tmp/ansible/${f}.yaml
+done
