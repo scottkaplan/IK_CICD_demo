7daf1a0fa9cb8553a15228a5b88df2f0addf00ff
Scott Kaplan <scott@kaplans.com>
2023-12-25 04:44:06 +0000

    Install and run ansible on bastion

--- terraform/11-bastion.tf
+++ terraform/11-bastion.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "IK-bastion" {
   ami           = "ami-0082110c417e4726e"
-  instance_type = "t3.micro"
+  instance_type = "t3.medium"
   key_name = "IK"
   iam_instance_profile = "IK-ec2"
   subnet_id = "subnet-0f4c16c45b98ab17e"
@@ -8,5 +8,17 @@ resource "aws_instance" "IK-bastion" {
 
   tags = {
     Name = "IK-bastion"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      sudo yum -y install ansible
+      mkdir /tmp/ansible
+      cd /tmp/ansible; wget https://raw.githubusercontent.com/scottkaplan/IK_CICD_demo/main/ansible/dot_files.yaml
+      cd /tmp/ansible; wget https://raw.githubusercontent.com/scottkaplan/IK_CICD_demo/main/ansible/jenkins.yaml
+      cd /tmp/ansible; wget https://raw.githubusercontent.com/scottkaplan/IK_CICD_demo/main/ansible/kubectl.yaml
+      cd /tmp/ansible; wget https://raw.githubusercontent.com/scottkaplan/IK_CICD_demo/main/ansible/packages.yaml
+      sudo ansible-playbook /tmp/ansible/*.yaml
+    EOT
   }
 }
