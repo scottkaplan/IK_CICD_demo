9e1e08482fdf981cb6db534622e2f09ab827c3a7
Scott Kaplan <scott@kaplans.com>
2023-12-26 21:06:18 +0000

    Use ssh to run setup script remotely on target.

--- terraform/11-bastion.tf
+++ terraform/11-bastion.tf
@@ -3,17 +3,25 @@ resource "aws_instance" "IK-bastion" {
   instance_type = "t3.medium"
   key_name = "IK"
   iam_instance_profile = "IK-ec2"
-  subnet_id = "subnet-0f4c16c45b98ab17e"
+  subnet_id = aws_subnet.public-us-west-1a.id
   vpc_security_group_ids = ["sg-04ce72d63382afeb6"]
 
   tags = {
     Name = "IK-bastion"
   }
 
-  provisioner "local-exec" {
-    command = <<EOT
-      /usr/bin/wget -O /tmp/config_bastion.sh https://raw.githubusercontent.com/scottkaplan/IK_CICD_demo/main/ansible/config_bastion.sh
-      /bin/bash /tmp/config_bastion.sh
-    EOT
+  connection {
+    type = "ssh"
+    host = aws_instance.IK-bastion.public_ip
+    user = "ec2-user"
+    private_key = "${file("/home/ec2-user/.ssh/IK.pem")}"
+    agent = true
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "/usr/bin/wget -O /tmp/config_bastion.sh https://raw.githubusercontent.com/scottkaplan/IK_CICD_demo/main/ansible/config_bastion.sh",
+      "/bin/bash /tmp/config_bastion.sh"
+    ]
   }
 }
