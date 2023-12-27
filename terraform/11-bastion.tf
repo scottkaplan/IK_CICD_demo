b9700ab52b9566d73454a7f0a4b144e1a00a5642
Scott Kaplan <scott@kaplans.com>
2023-12-27 23:29:40 +0000

    Create the bastion SG.

--- terraform/11-bastion.tf
+++ terraform/11-bastion.tf
@@ -1,10 +1,60 @@
+resource "aws_security_group" "bastion" {
+  name        = "bastion"
+  description = "Allow SSH, HTTP(S), Jenkins"
+  vpc_id      = aws_vpc.IK.id
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "Jenkins"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "bastion"
+  }
+}
+
 resource "aws_instance" "IK-bastion" {
   ami           = "ami-0082110c417e4726e"
   instance_type = "t3.medium"
   key_name = "IK"
   iam_instance_profile = "IK-ec2"
   subnet_id = aws_subnet.public-us-west-1a.id
-  vpc_security_group_ids = ["sg-04ce72d63382afeb6"]
+  vpc_security_group_ids = [aws_security_group.bastion.id]
 
   tags = {
     Name = "IK-bastion"
