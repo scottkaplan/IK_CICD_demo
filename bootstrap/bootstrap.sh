dfd2028821be3b3971c4509698eb386ad70171a1
Scott Kaplan <scott@kaplans.com>
2023-12-28 13:17:50 -0800

    Fix bootstrap script.

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -2,23 +2,21 @@
 
 sudo apt -y update
 sudo apt -y upgrade
+sudo apt -y install curl
+
+# install latest AWS CLI
+cd /tmp
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 aws --version
 
-# download GPG key
-curl https://apt.releases.hashicorp.com/gpg | gpg — dearmor > hashicorp.gpg
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-
-# Install new repo
+# Install terraform
+cd /tmp
+curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp.gpg
 sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
-sudo apt-add-repository “deb [arch=$(dpkg — print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt install terraform
-
 terraform --version
 
 echo "Configure AWS credentials"
