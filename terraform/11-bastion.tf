2f0b2f26e6f6c96175e52e297e495bf348a12ca4
Scott Kaplan <scott@kaplans.com>
2023-12-28 20:10:50 +0000

    Fix DNS config bug

--- terraform/11-bastion.tf
+++ terraform/11-bastion.tf
@@ -97,9 +97,21 @@ resource "aws_instance" "IK-bastion" {
   }
 }
 
+resource "aws_route53_zone" "kaplans" {
+  name = "kaplans.com"
+}
+
 resource "aws_route53_record" "ik-bastion" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = aws_route53_zone.kaplans.zone_id
   name    = "ik-bastion.kaplans.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.IK-bastion.public_ip]
+}
+
+resource "aws_route53_record" "ik-jenkins" {
+  zone_id = aws_route53_zone.kaplans.zone_id
+  name    = "ik-jenkins.kaplans.com"
   type    = "A"
   ttl     = 300
   records = [aws_instance.IK-bastion.public_ip]
