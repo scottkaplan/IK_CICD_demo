b3b2a82de626b40f2f83d8c56c5b29163d461359
Scott Kaplan <scott@kaplans.com>
2024-01-04 14:55:11 -0800

    clean cruft.

--- terraform/6-lb.tf
+++ terraform/6-lb.tf
@@ -36,11 +36,6 @@ locals {
   lb_name = split("-", split(".", kubernetes_service.example.status.0.load_balancer.0.ingress.0.hostname).0).0
 }
 
-# Read information about the load balancer using the AWS provider.
-# data "aws_elb" "example" {
-#   name = local.lb_name
-# }
-
 output "load_balancer_name" {
   value = local.lb_name
 }
@@ -48,7 +43,3 @@ output "load_balancer_name" {
 output "load_balancer_hostname" {
   value = kubernetes_service.example.status.0.load_balancer.0.ingress.0.hostname
 }
-
-# output "load_balancer_info" {
-#   value = data.aws_elb.example
-# }
