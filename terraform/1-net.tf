5a185108d4750eb0c85a18619d45516ea0a3347c
Scott Kaplan <scott@kaplans.com>
2024-01-05 13:06:56 -0800

    reorganize file names

--- terraform/1-net.tf
+++ terraform/1-net.tf
@@ -167,3 +167,48 @@ resource "aws_route_table_association" "public-us-west-1c" {
   subnet_id      = aws_subnet.public-us-west-1c.id
   route_table_id = aws_route_table.public.id
 }
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.demo.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.demo.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
+  }
+}
+
+resource "kubernetes_service" "demo" {
+  metadata {
+    name = "demo"
+    annotations = {
+      "service.beta.kubernetes.io/aws-load-balancer-name" = "demo"
+      "service.beta.kubernetes.io/aws-load-balancer-type" = "nlb"
+      "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "instance"
+      "service.beta.kubernetes.io/load-balancer-source-ranges" = "0.0.0.0/0"
+      "service.beta.kubernetes.io/aws-load-balancer-scheme": "internet-facing"
+    }
+  }
+  spec {
+    selector = {
+      app = "web"
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+    type = "LoadBalancer"
+  }
+}
+
+resource "aws_route53_record" "ik-k8s" {
+  zone_id = data.aws_route53_zone.kaplans.zone_id
+  name    = "ik-k8s.kaplans.com"
+  type    = "CNAME"
+  ttl     = 300
+  records = [kubernetes_service.demo.status.0.load_balancer.0.ingress.0.hostname]
+}
+
+output "load_balancer_hostname" {
+  value = kubernetes_service.demo.status.0.load_balancer.0.ingress.0.hostname
+}
